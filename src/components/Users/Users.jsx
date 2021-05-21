23cd844c4ec8f0cc5db408f3cf3b69ccf97da2eb
alvikalvik <alvikalvik@gmail.com>
2021-05-21 19:30:26 +0300

    53 step done (Users class component refactored)

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,64 +1,55 @@
+import {Component} from 'react';
 import User from "./User/User"
 import * as axios from 'axios';
 
-const Users = (props) => {
-    //Tempoary IF !!!
-    if (props.users.length === 0) {        
+class Users extends Component {
+    constructor(props) {
+        super(props);
         axios.get('https://social-network.samuraijs.com/api/1.0/users')
             .then( response => {                
                 props.setUsers(response.data.items);
             });
-        // props.setUsers([
-        //     {
-        //         id: 1,
-        //         fullName: 'User 1',
-        //         photo: 'https://www.byrdie.com/thmb/Jjj54SaUGDzwW7h5MxQI8hsNXnw=/1000x1000/filters:fill(auto,1)/Stocksy_txp67ff3d0fWrz200_Medium_3514532-crop-29154df0578b40309acce7c668e12faa.jpg',
-        //         status: 'Status message',
-        //         isFollowed: false,
-        //         location: {
-        //             country: 'Ukraine',
-        //             city: 'Kiev'
-        //         }
-        //     },
-        //     {
-        //         id: 2,
-        //         fullName: 'User 2',
-        //         photo: 'https://www.bioid.com/wp-content/uploads/face-database-bioid.jpg',
-        //         status: 'Status message 2',
-        //         isFollowed: true,
-        //         location: {
-        //             country: 'Ukraine',
-        //             city: 'Lviv'
-        //         }
-        //     },
-        //     {
-        //         id: 3,
-        //         fullName: 'USER 3',
-        //         photo: 'https://static-bebeautiful-in.unileverservices.com/Unlock-flawless-skin_MobileHomeFeature.jpg',
-        //         status: 'Status message 3',
-        //         isFollowed: false,
-        //         location: {
-        //             country: 'USA',
-        //             city: 'MIAMI'
-        //         }
-        //     },        
-        // ]);
+    }
+    
+    render() {
+        const usersArray = this.props.users.map( u => {
+            return <User key={u.id}
+                        userData={u}                
+                        follow={this.props.follow}
+                        unfollow={this.props.unfollow}                
+                    />;
+        });
+        return (
+            <div>
+                {usersArray}
+            </div>
+        );
     }
 
-
-
-    const usersArray = props.users.map( u => {
-        return <User key={u.id}
-                    userData={u}                
-                    follow={props.follow}
-                    unfollow={props.unfollow}                
-                />;
-    });
-    return (
-        <div>
-            {usersArray}
-        </div>
-    );
+    
 }
+
+// const Users = (props) => {
+//     //Tempoary IF !!!
+//     if (props.users.length === 0) {        
+//         axios.get('https://social-network.samuraijs.com/api/1.0/users')
+//             .then( response => {                
+//                 props.setUsers(response.data.items);
+//             });  
+//     }
+    
+//     const usersArray = props.users.map( u => {
+//         return <User key={u.id}
+//                     userData={u}                
+//                     follow={props.follow}
+//                     unfollow={props.unfollow}                
+//                 />;
+//     });
+//     return (
+//         <div>
+//             {usersArray}
+//         </div>
+//     );
+// }
 
 export default Users;
