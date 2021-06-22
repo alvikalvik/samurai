1083916425ba448c1533ef88c69afbff6e088be5
alvikalvik <alvikalvik@gmail.com>
2021-06-22 21:36:30 +0300

    94 step done (React.lazy implemented for music component)

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
+import React, { Suspense } from 'react';
 import "./App.css";
 import HeaderContainer from "./components/Header/HeaderContainer";
 import NavbarContainer from "./components/Navbar/NavbarContainer";
 import News from "./components/News/News";
-import Music from "./components/Music/Music";
 import Settings from "./components/Settings/Settings";
 import {Route, withRouter} from 'react-router-dom';
 import DialogsContainer from "./components/Dialogs/DialogsContainer";
@@ -16,6 +16,8 @@ import {
     initialize    
 } from './redux/appReduser';
 import { compose } from "redux";
+
+const Music = React.lazy(() => import('./components/Music/Music'));
 
 
 class App extends Component {
@@ -33,27 +35,29 @@ class App extends Component {
                 <HeaderContainer />
                 <NavbarContainer />
                 <main className="app-content">
-                    <Route path="/profile/:userId?">
-                        <ProfileContainer />
-                    </Route>
-                    <Route path="/dialogs">
-                        <DialogsContainer />
-                    </Route>
-                    <Route path="/news">
-                        <News />
-                    </Route>
-                    <Route path="/music">
-                        <Music />
-                    </Route>
-                    <Route path="/users">
-                        <UsersContainer />
-                    </Route>
-                    <Route path="/settings">
-                        <Settings />
-                    </Route>
-                    <Route path="/login">
-                        <LoginContainer />
-                    </Route>
+                    <Suspense fallback={<Preloader />}>
+                        <Route path="/profile/:userId?">
+                            <ProfileContainer />
+                        </Route>
+                        <Route path="/dialogs">
+                            <DialogsContainer />
+                        </Route>
+                        <Route path="/news">
+                            <News />
+                        </Route>
+                        <Route path="/music">
+                            <Music />
+                        </Route>
+                        <Route path="/users">
+                            <UsersContainer />
+                        </Route>
+                        <Route path="/settings">
+                            <Settings />
+                        </Route>
+                        <Route path="/login">
+                            <LoginContainer />
+                        </Route>
+                    </Suspense>
                 </main>				
             </div>
         );
